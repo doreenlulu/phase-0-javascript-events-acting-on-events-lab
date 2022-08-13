c9074c7e11ee2b8b10ca4a29bb316ced8c6abfb3
Doreen <doreenlulu97@gmail.com>
2022-08-13 17:01:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+	let leftValue = dodger.style.left.replace("px", "");
+
+	let leftInt = parseInt(leftValue, 10);
+
+	if (leftInt > 0) {
+		dodger.style.left = `${leftInt - 1}px`;
+	}
+}
+
+function moveDodgerRight() {
+	var rightvalue = dodger.style.left.replace("px", "");
+
+	var rightInt = parseInt(rightvalue, 10);
+
+	if (rightInt < 360) {
+		dodger.style.left = `${rightInt + 1}px`;
+	}
+}
+
+document.addEventListener("keydown", function (e) {
+	if (e.which === 20) {
+		moveDodgerLeft();
+	}
+	if (e.which === 30) {
+		moveDodgerRight();
+	}
+});
